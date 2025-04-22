180279658d47406ba011f685cdff5b0a0cae7e90
Sai Bharath <130722804+118bharath@users.noreply.github.com>
2025-04-22 19:30:12 +0000

    Remodified the Hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,43 +1,23 @@
-import React from 'react'
-import { Spotlight } from './ui/spotlight-new'
-import { TextGenerateEffect } from './ui/text-generate-effect'
-import clsx from 'clsx'
+"use client";
+import React from "react";
+import { Spotlight } from "@/components/ui/spotlight-new";
+import { IconCloudDemo } from "./ui/IconCloud";
 
-
-const Hero = () => {
+export default function Hero() {
   return (
-    <div className='pb-20 pt-36'>
-      
-      <div>
-      <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white" />
-      <Spotlight className="top-10 left-full w-[50vw] h-screen" fill="purple" />
-      <Spotlight className="top-28 left-80 h-screen w-[50vw]" fill="blue" />
-      </div>
-      <div className="relative flex h-screen w-full items-center justify-center bg-white dark:bg-black">
-      <div
-        className={clsx(
-        "absolute inset-0",
-        "[background-size:40px_40px]",
-        "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
-        "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]",
-        )}
-      />
-      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
-      <div className="relative z-20 flex flex-col items-center">
-        <p className="text-md text-black dark:text-blue-400 sm:text-4xl">
-          Hi, I'm Sai Bharath
+    <div className="min-h-screen w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+      <Spotlight />
+      <div className="p-4 max-w-7xl mx-auto relative z-10 w-full pt-20 md:pt-0">
+        <h1 className="pr-10 text-3xl md:text-6xl font-bold text-left bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+          Transforming <br /> Ideas into Digital Experiences
+        </h1>
+        <p className="mt-4 font-normal text-sm text-neutral-300 max-w-lg text-left">
+          Hi, I'm Sai Bharath Vemula a CS Graduate Driving Innovation Through Scalable Web Development
         </p>
-        <p className="text-4xl font-bold text-black dark:text-white sm:text-7xl">
-          Software Developer
-        </p>
-        <TextGenerateEffect
-          className="text-2xl text-white dark:text-white"
-          words="Crafting Digital Experiences"
-        />
-      </div>
-      </div>
+            </div>
+            <div className="pr-[150px] scale-150">
+              <IconCloudDemo/>
+            </div>
     </div>
-  )
+  );
 }
-
-export default Hero
